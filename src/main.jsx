baadf4a74ed58cc38d9f29571d1da2df007eef58
2300030144 <2300030144@kluniversity.in>
2025-09-23 00:29:03 +0530

    Add built files for gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./components/HomePage";
-import SignUpPage from "./components/Signup";
-import ExploreArt from "./components/ExploreArt";
-import Dashboard from "./components/Dashboard"; // Add Dashboard import
-import "./index.css"; // Ensure global styles if needed
-import Signup from "./components/Signup";
-import Login from "./components/Login";
-import VirtualSpace from "./components/VirtualSpace";
-import AuctionCard from "./components/AuctionCard";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/explore" element={<ExploreArt />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/virtualspace" element={<VirtualSpace />} />
-      <Route path="/auctioncard" element={<AuctionCard />} />
- 
-    </Routes>
-  </Router>
+  <React.StrictMode>
+    <Router basename="/frontend-echosofheart">
+      <App />
+    </Router>
+  </React.StrictMode>
 );
